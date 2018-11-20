31968afceb137dfa6d4eaeb062425cde2e67eb2a
EberthFelipe <eberthfelipe@gmail.com>
2018-11-20 19:37:03 -0400

    Complete exercise

--- app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -16,11 +16,14 @@
 
 package com.example.android.android_me.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.example.android.android_me.R;
@@ -34,9 +37,18 @@ public class MasterListFragment extends Fragment {
     // TODO (1) Define a new interface OnImageClickListener that triggers a callback in the host activity
         // The callback is a method named onImageSelected(int position) that contains information about
         // which position on the grid of images a user has clicked
+    OnImageClickListener mCallBack;
 
     // TODO (2) Override onAttach to make sure that the container activity has implemented the callback
-
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mCallBack = (OnImageClickListener) context;
+        } catch (ClassCastException exc) {
+            throw new ClassCastException(context.toString() + "must implements OnImageClickListener");
+        }
+    }
 
     // Mandatory empty constructor
     public MasterListFragment() {
@@ -60,9 +72,18 @@ public class MasterListFragment extends Fragment {
         gridView.setAdapter(mAdapter);
 
         // TODO (3) Set a click listener on the gridView and trigger the callback onImageSelected when an item is clicked
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mCallBack.onImageSelected(position);
+            }
+        });
 
         // Return the root view
         return rootView;
     }
 
+    public interface OnImageClickListener {
+        void onImageSelected(int position);
+    }
 }
